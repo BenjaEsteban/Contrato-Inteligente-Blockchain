a258ee4d1668d9664754cace882c0fe808143611
Benjamin Rodriguez <b.esteban.r.v@gmail.com>
2023-12-03 22:07:29 -0300

    [ADD] state contrat function and view contract function

--- compraVenta.sol
+++ compraVenta.sol
@@ -5,29 +5,51 @@ contract ContratoCompraVenta {
     address public vendedor;
     address public comprador;
     string public descripcion;
-    uint public precio;
+    uint256 public precio;
     bool public contratoFirmado;
-    uint public numeroSecreto; 
+    uint256 public numeroSecreto;
+    bool public contratoGuardado;
 
-    event ContratoCreado(address indexed vendedor, string descripcion, uint precio);
+    event ContratoCreado(
+        address indexed vendedor,
+        string descripcion,
+        uint256 precio
+    );
     event ContratoFirmado(address indexed comprador);
+    event ContratoGuardado();
 
     modifier contratoNoFirmado() {
         require(!contratoFirmado, "El contrato ya ha sido firmado");
         _;
     }
 
-    modifier soloVendedor() {
-        require(msg.sender == vendedor, "Solo el vendedor puede llamar a esta funcion");
+    modifier contratoFirmadoYNoGuardado() {
+        require(
+            contratoFirmado && !contratoGuardado,
+            "El contrato no ha sido firmado o ya ha sido guardado"
+        );
         _;
     }
 
     constructor() {
-
+        // Constructor
     }
 
-    function inicializarContrato(string memory _descripcion, uint _precio, uint _numeroSecreto) external {
+    function reiniciarContrato() external {
+        require(msg.sender == vendedor, "Solo el vendedor puede reiniciar el contrato");
+        vendedor = address(0);
+        comprador = address(0);
+        descripcion = "";
+        precio = 0;
+        contratoFirmado = false;
+        numeroSecreto = 0;
+        contratoGuardado = false;
+    }
+
+    function inicializarContrato(string memory _descripcion, uint256 _precio, uint256 _numeroSecreto) external {
         require(!contratoFirmado, "El contrato ya ha sido firmado");
+        require(vendedor == address(0), "El contrato ya ha sido inicializado");
+        require(msg.sender != address(0), "La direccion del vendedor no es valida");
 
         descripcion = _descripcion;
         precio = _precio;
@@ -37,7 +59,7 @@ contract ContratoCompraVenta {
         emit ContratoCreado(vendedor, descripcion, precio);
     }
 
-    function firmarContrato(uint _numeroSecreto) external contratoNoFirmado {
+    function firmarContrato(uint256 _numeroSecreto) external contratoNoFirmado {
         require(_numeroSecreto == numeroSecreto, "Numero secreto incorrecto");
 
         comprador = msg.sender;
@@ -46,8 +68,22 @@ contract ContratoCompraVenta {
         emit ContratoFirmado(comprador);
     }
 
-    function verDetalleContrato() external view returns (string memory, uint) {
-        require(contratoFirmado, "El contrato aun no ha sido firmado");
-        return (descripcion, precio);
+    function guardarContratoEnBlockchain() external contratoFirmadoYNoGuardado {
+        contratoGuardado = true;
+        emit ContratoGuardado();
+    }
+
+    function verDetalleContrato() external view returns (string memory, uint256, address, address) {
+        return (descripcion, precio, vendedor, comprador);
+    }
+
+    function obtenerEstadoContrato() external view returns (string memory) {
+        if (!contratoFirmado) {
+            return "Pendiente de firma";
+        } else if (contratoFirmado && !contratoGuardado) {
+            return "Firmado, pendiente de guardar";
+        } else {
+            return "Firmado y guardado";
+        }
     }
 }
